3828e44c2544f33fa03f6617ab68079088156a9c
taner.acar <taneracar1@icloud.com>
2025-11-27 15:47:41 +0300

    add more fabric textures

--- src/components/TshirtV2.tsx
+++ src/components/TshirtV2.tsx
@@ -7,7 +7,12 @@ import bgImage1 from "../../public/images/bgImage1.jpg";
 import bgImage2 from "../../public/images/bgImage2.jpg";
 import bgImage3 from "../../public/images/bgImage3.jpg";
 import bgImage4 from "../../public/images/bgImage4.jpg";
+import bgImage5 from "../../public/images/bgImage5.jpg";
+import bgImage6 from "../../public/images/bgImage6.jpg";
+import bgImage7 from "../../public/images/bgImage7.jpg";
+import bgImage8 from "../../public/images/bgImage9.jpg";
 import CustomImage from "./customImage";
+import IstcodeLogo from "./IstcodeLogo";
 //import Image from "next/image";
 
 export default function TshirtCanvas() {
@@ -107,7 +112,8 @@ export default function TshirtCanvas() {
   };
 
   return (
-    <div className="flex items-center flex-col lg:flex-row gap-4 w-full bg-white justify-between lg:pl-[5vw] h-fit lg:h-screen">
+    <div className="flex items-center flex-col lg:flex-row gap-4 w-full bg-white justify-between lg:pl-[5vw] h-fit lg:h-screen relative overflow-x-hidden">
+      <div className="absolute w-full items-center h-fit top-8 left-12 hidden lg:flex"><IstcodeLogo /></div>
       <div className="w-[90vw] lg:w-[50%] h-fit lg:h-full flex items-center justify-center overflow-x-hidden max-lg:mt-[50px]">
         <canvas ref={canvasRef} width={500} height={500} className="w-full h-fit lg:h-full" />
       </div>
@@ -116,13 +122,20 @@ export default function TshirtCanvas() {
           <button onClick={() => changeColor("#f02c37")} className="px-4 py-2 bg-red-500 text-white w-[10vw] h-[10vw] rounded-4xl cursor-pointer">Red</button>
           <button onClick={() => changeColor("#4a7fff")} className="px-4 py-2 bg-blue-500 text-white w-[10vw] h-[10vw] rounded-4xl cursor-pointer">Blue</button>
           <button onClick={() => changeColor("#40c950")} className="px-4 py-2 bg-green-500 text-white w-[10vw] h-[10vw] rounded-4xl cursor-pointer">Green</button>
-          <button onClick={() => changeColor("#f0b100")} className="px-4 py-2 bg-yellow-500 text-black w-[10vw] h-[10vw] rounded-4xl cursor-pointer">Yellow</button>
+          <button onClick={() => changeColor("#f0b100")} className="px-4 py-2 bg-yellow-500 text-white w-[10vw] h-[10vw] rounded-4xl cursor-pointer">Yellow</button>
         </div>
         <div className="flex gap-2">
           <button onClick={() => changeBackgroundImage(bgImage1.src)} className="w-[10vw] h-[10vw] rounded-4xl cursor-pointer"><CustomImage src={bgImage1.src} /></button>
           <button onClick={() => changeBackgroundImage(bgImage2.src)} className="w-[10vw] h-[10vw] rounded-4xl cursor-pointer"><CustomImage src={bgImage2.src}  /></button>
                     <button onClick={() => changeColor("#6e3338")} className="w-[10vw] h-[10vw] rounded-4xl cursor-pointer"><CustomImage src={bgImage3.src}  /></button>
                     <button onClick={() => changeColor("#d4c4ac")} className="w-[10vw] h-[10vw] rounded-4xl cursor-pointer"><CustomImage src={bgImage4.src}  /></button>
+        </div>
+                <div className="flex gap-2">
+          <button onClick={() => changeBackgroundImage(bgImage5.src)} className="w-[10vw] h-[10vw] rounded-4xl cursor-pointer"><CustomImage src={bgImage5.src} /></button>
+           <button onClick={() => changeBackgroundImage(bgImage6.src)} className="w-[10vw] h-[10vw] rounded-4xl cursor-pointer"><CustomImage src={bgImage6.src} /></button>
+            <button onClick={() => changeBackgroundImage(bgImage7.src)} className="w-[10vw] h-[10vw] rounded-4xl cursor-pointer"><CustomImage src={bgImage7.src} /></button>
+             <button onClick={() => changeBackgroundImage(bgImage8.src)} className="w-[10vw] h-[10vw] rounded-4xl cursor-pointer"><CustomImage src={bgImage8.src} /></button>
+    
         </div>
       </div>
     </div>
